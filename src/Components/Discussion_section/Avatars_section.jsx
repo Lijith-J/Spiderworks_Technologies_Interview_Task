c6dbfe09ad7eaa087e732eed40915fa9cc40fd6f
Lijith <lijithlj786@gmail.com>
2024-05-30 03:49:30 +0530

    changes in avatars

--- src/Components/Discussion_section/Avatars_section.jsx
+++ src/Components/Discussion_section/Avatars_section.jsx
@@ -2,8 +2,14 @@ import React, { useEffect } from 'react'
 import '../../Styles/Styles.scss'
 
 import avatar1 from './images/2151100226.jpg'
-import avatar2 from './images/50955.jpg'
-import avatar3 from './images/freepik-export-20240529133139mVT3.png'
+import avatar2 from './images/freepik-export-20240529133139mVT3.png'
+import avatar3 from './images/50955.jpg'
+import avatar4 from './images/2151100205.jpg'
+import avatar5 from './images/40542.jpg'
+import avatar6 from './images/51154.jpg'
+import avatar7 from './images/51267.jpg'
+import avatar8 from './images/51725.jpg'
+
 
 import Aos from 'aos'
 import 'aos/dist/aos.css'
@@ -33,7 +39,7 @@ const Avatars_section = () => {
 
                     <div className='bg-white p-2 w-[70px] sm:w-[100px] md:w-[140px] lg:w-[170px] h-[70px] sm:h-[100px] md:h-[140px] lg:h-[170px] rounded-full flex  avatar-bubbles-div-2'>
                         <div className=' bg-yellow-300 rounded-full flex justify-center items-center '>
-                            <img src={avatar3} alt="" />
+                            <img src={avatar2} alt="" />
                         </div>
                         <div className='bg-teal-700 text-white text-sm px-4 py-[2px]'>
                             How?
@@ -45,13 +51,13 @@ const Avatars_section = () => {
                 <div className=' w-[100%] h-[100%] grid justify-evenly items-center ' >
 
                     <div className='bg-white p-2 w-[70px] sm:w-[100px] md:w-[140px] lg:w-[170px] h-[70px] sm:h-[100px] md:h-[140px] lg:h-[170px] rounded-full  avatar-bubbles-div-3'>
-                        <div className=' bg-teal-500 rounded-full flex justify-center items-center overflow-hidden'>
+                        <div className='  rounded-full flex justify-center items-center overflow-hidden'>
                             <img src={avatar3} alt="" />
                         </div>
                     </div>
                     <div className='bg-white p-2 w-[70px] sm:w-[100px] md:w-[140px] lg:w-[170px] h-[70px] sm:h-[100px] md:h-[140px] lg:h-[170px] rounded-full flex  avatar-bubbles-div-4'>
                         <div className=' rounded-full flex justify-center items-center overflow-hidden'>
-                            <img src={avatar1} alt="" />
+                            <img src={avatar4} alt="" />
                         </div>
                         <div className='bg-blue-500 text-white text-sm px-3 sm:px-5 py-[2px]'>
                             That's it! Sir.
@@ -64,12 +70,12 @@ const Avatars_section = () => {
 
                     <div className='bg-white p-2 w-[70px] sm:w-[100px] md:w-[140px] lg:w-[170px] h-[70px] sm:h-[100px] md:h-[140px] lg:h-[170px] rounded-full  avatar-bubbles-div-5'>
                         <div className=' rounded-full flex justify-center items-center overflow-hidden'>
-                            <img src={avatar2} alt="" />
+                            <img src={avatar5} alt="" />
                         </div>
                     </div>
                     <div className='bg-white p-2 w-[70px] sm:w-[100px] md:w-[140px] lg:w-[170px] h-[70px] sm:h-[100px] md:h-[140px] lg:h-[170px] rounded-full  avatar-bubbles-div-6'>
-                        <div className=' bg-blue-500 rounded-full flex justify-center items-center overflow-hidden'>
-                            <img src={avatar3} alt="" />
+                        <div className='  rounded-full flex justify-center items-center overflow-hidden'>
+                            <img src={avatar6} alt="" />
                         </div>
                         <div className='bg-yellow-400 text-white text-sm px-5 py-[2px]'>
                             Hey Thanks
@@ -82,12 +88,12 @@ const Avatars_section = () => {
 
                     <div className='bg-white p-2 w-[70px] sm:w-[100px] md:w-[140px] lg:w-[170px] h-[70px] sm:h-[100px] md:h-[140px] lg:h-[170px] rounded-full  avatar-bubbles-div-7'>
                         <div className=' rounded-full flex justify-center items-center overflow-hidden'>
-                            <img src={avatar1} alt="" />
+                            <img src={avatar7} alt="" />
                         </div>
                     </div>
                     <div className='bg-white p-2 w-[70px] sm:w-[100px] md:w-[140px] lg:w-[170px] h-[70px] sm:h-[100px] md:h-[140px] lg:h-[170px] rounded-full  avatar-bubbles-div-8'>
                         <div className=' rounded-full flex justify-center items-center overflow-hidden'>
-                            <img src={avatar2} alt="" />
+                            <img src={avatar8} alt="" />
                         </div>
                         <div className='bg-orange-500 text-white text-sm px-3 py-[2px]'>
                             Thats's Awesome
