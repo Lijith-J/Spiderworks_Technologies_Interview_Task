a33ebedf76a9b1665e693fca174b4efd1a4ab722
Lijith <lijithlj786@gmail.com>
2024-05-30 15:36:37 +0530

    desktop view finished

--- src/Components/All_Topics_secion/All_Topics_Section_Main.jsx
+++ src/Components/All_Topics_secion/All_Topics_Section_Main.jsx
@@ -6,14 +6,14 @@ import Contributors_section from './Contributors_section'
 const All_Topics_Section_Main = () => {
   return (
     <>
-        <div className='flex justify-between bg-red-400 py-28'>
-            <div className='bg-yellow-400 p-7 px-20 w-[68%] flex flex-col'>
+        <div className='flex justify-between bg-red-400  py-22'>
+            <div className=' p-7 px-20 w-[68%] flex flex-col'>
                 <SearchTopics/>
                 <Question_Answer_section/>
             </div>
 
-            <div className='bg-blue-400 w-[28%]'>
-                <Contributors_section/>
+            <div className=' w-[28%] flex'>
+                <Contributors_section/> 
             </div>
         </div>
 
--- src/Components/All_Topics_secion/Contributors_section.jsx
+++ src/Components/All_Topics_secion/Contributors_section.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 import { MdStar } from "react-icons/md";
 import { PiStarFourFill } from "react-icons/pi";
+import { MdArrowRightAlt } from "react-icons/md";
 
-import picture1 from './images/bearded-man-with-striped-shirt.jpg'
-import picture2 from './images/close-up-portrait-curly-handsome-european-male.jpg'
-import pic from './images/WhatsApp_Image_2024-05-30_at_2.49.48_PM-removebg-preview.png'
+import picture1 from './images/close-up-portrait-curly-handsome-european-male.jpg'
+import picture2 from './images/WhatsApp_Image_2024-05-30_at_2.49.48_PM-removebg-preview.png'
 
 
 import { BiMessageRounded } from "react-icons/bi";
@@ -15,7 +15,9 @@ const Contributors_section = () => {
   return (
     <>
 
-      <div className=' rounded-2xl bg-red-50 flex flex-col justify-center py-10'>
+    <div className='p-2  bg-blue-400 flex flex-col justify-center gap-20'>
+
+      <div className=' rounded-2xl bg-red-100 flex flex-col justify-center py-10'>
 
         <div className=' py-4 contributor-top-div'>
 
@@ -34,7 +36,7 @@ const Contributors_section = () => {
             </div>
 
             <div className=' w-[170px] h-[170px] flex justify-center object-fill overflow-hidden rounded-full'>
-              <img src={picture1} alt="" />
+              <img src={picture1} className='object-cover' alt="" />
             </div>
 
             <div className='absolute bg-teal-800 text-white top-contributor-rating-tag'>
@@ -56,9 +58,8 @@ const Contributors_section = () => {
         <div className='p-3 px-10 flex flex-col gap-3 items-center'>
           <p>You can be next one! Be a part of USA's 1st CPT Program Directory</p>
 
-          <button className='flex gap-1 px-[3.5rem] py-[1.5rem] rounded-full text-white join-community-button'>
+          <button className='flex gap-1 px-[3rem] py-[1.5rem] rounded-full text-white join-community-button'>
             Join Community & Ask
-
             <PiStarFourFill color='gold' />
           </button>
         </div>
@@ -66,30 +67,35 @@ const Contributors_section = () => {
       </div>
 
 
+      {/* Bottom Part ===================================================================== */}
 
-      <div className='  p-4 contibutors-bottom-parent-div'>
+      <div className=' rounded-2xl p-4 contibutors-bottom-parent-div'>
 
         <div className='  w-full h-[300px] p-5 flex justify-center items-center'>
 
-          <div className='backside-image-rounded-div1'>
+          <div className='backside-image-rounded-div1'></div>
+          <div className='backside-image-rounded-div2'></div>
 
-          </div>
-          <div className='backside-image-rounded-div2'>
-
-          </div>
           <div className=' backside-image-rounded-div3'>
-            <img src={pic} alt="" />
-
+            <img src={picture2} alt="" />
           </div>
 
         </div>
 
-        <div>
-          <span>E-BOOK</span>
-
-
+        <div className='  text-white p-2 flex gap-3 flex-col items-center'>
+          <span className='text-sm'>E-BOOK</span>
+          <p className=' text-[18px] text-center font-serif'>Charting Paths:Program Through a <br /> <span className='italic'>Student's Eyes</span></p>
         </div>
 
+        <div className='p-3 flex justify-center items-center'>
+          <button className=' text-white ring-1 ring-white px-4 py-2 flex gap-1 items-center rounded-full hover:bg-white hover:text-stone-900 hover:ring-stone-900'>
+            Get the guide
+            <MdArrowRightAlt size={40} />
+          </button>
+        </div>
+
+
+      </div>
 
       </div>
 
